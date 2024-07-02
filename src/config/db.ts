11c6c1002e675772b073798f70bf25a9b40454c9
Guchito <a_gaggero@hotmail.com>
2024-07-02 02:46:35 +0200

    Creating the post method

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,15 +1,16 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize-typescript";
 import dotenv from 'dotenv'
 
 dotenv.config()
-console.log()
+
 
 const db = new Sequelize(process.env.DB_URL!,{
     dialectOptions: {
         ssl: {
             require: false
         }
-    }
+    },
+    models: [__dirname + '/../models/**/*.ts']
 })
 
 export default db
