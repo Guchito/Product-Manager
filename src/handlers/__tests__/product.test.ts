df0086026604d601e0132c59319afcc233e8c116
Guchito <a_gaggero@hotmail.com>
2024-07-18 21:07:41 +0200

    testing del endpoint delete

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -180,3 +180,36 @@ describe('PUT /api/products/:id', () =>{
         expect(response.body).not.toHaveProperty('errors')
     })
 })
+
+
+describe('DELETE /api/products/:id', () =>{
+
+    it('Should check a valid ID in the url', async() => {
+        const response = await request(server)
+            .delete('/api/products/not-valid-url')
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors[0].msg).toBe('ID not valid')
+    
+    })
+
+    it('should return a 404 response for a non-existing product', async () => {
+        const productId = 2000
+        const response = await request(server)
+            .delete(`/api/products/${productId}`)
+        expect(response.status).toBe(404)
+        expect(response.body.error).toBe('Product not found')
+
+        expect(response.status).not.toBe(200)
+
+    })
+
+    it('should delete a product', async () => {
+        const response = await request(server).delete('/api/products/1')
+        expect(response.status).toBe(200)
+        expect(response.body.data).toBe('product deleted')
+
+        expect(response.status).not.toBe(400)
+        expect(response.status).not.toBe(404)
+    })
+})
