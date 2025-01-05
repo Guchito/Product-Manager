924e398bbe6b17d5681d3340de4df3f2a28afab7
guchito <a_gaggero@hotmail.com>
2025-01-05 17:04:47 +0100

    Added mobile view

--- client/src/layouts/layout.tsx
+++ client/src/layouts/layout.tsx
@@ -4,7 +4,7 @@ export default function layout() {
   return (
     <>
         <header className="bg-slate-800">
-            <div className="mx-auto max-w-6xl py-10">
+            <div className="max-w-6xl py-10 mx-auto text-center">
                 <h1 className="text-4xl font-extrabold text-white">
                     Product Manager
                 </h1>
