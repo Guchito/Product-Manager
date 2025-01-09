ebc5f2b83b9bcc4f56a3bcc331d4649da194f548
guchito <a_gaggero@hotmail.com>
2025-01-10 00:01:02 +0100

    Added ssl to the db

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -4,10 +4,12 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 
+
 const db = new Sequelize(process.env.DB_URL!,{
     dialectOptions: {
         ssl: {
-            require: false
+            require: true, // Enforce SSL
+            rejectUnauthorized: false, // Allow self-signed certificates (if needed)
         }
     },
     models: [__dirname + '/../models/**/*'],
